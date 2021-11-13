d62b752aae67a280628508d1bdf10e443a591b3f
atiQkhalid <39992488+atiQkhalid@users.noreply.github.com>
2021-11-14 01:00:36 +0500

    I added extention function for replacingfragment

--- app/src/main/java/com/example/sampleprojectforrecruitment/extenssions/FragmentExtension.kt
+++ app/src/main/java/com/example/sampleprojectforrecruitment/extenssions/FragmentExtension.kt
@@ -31,19 +31,29 @@ fun AppCompatActivity.replaceFragmentSafely(
     }
 }
 
-
-fun Fragment.replaceFragmentInFragment(
-    container: View,
+/**
+ * Extension function to replace Fragment Safely from a Fragment
+ * @param fragment destination Fragment
+ * @param addToBackStack as Boolean default value is true
+ * @param bundle as Bundle could be null
+ * @return void
+ */
+fun Fragment.replaceFragment(
     fragment: Fragment,
     addToBackStack: Boolean = true,
-    bundle: Bundle? = null
+    bundle: Bundle? = null,
+    shouldReplace: Boolean = true
 ) {
     val transaction =
         this.parentFragmentManager.beginTransaction()
     if (addToBackStack) {
         transaction.addToBackStack(null)
     }
-    transaction.replace(container.id, fragment, fragment.javaClass.name)
+    if (shouldReplace)
+        transaction.replace(R.id.container, fragment, fragment.javaClass.name)
+    else
+        transaction.add(R.id.container, fragment, fragment.javaClass.name)
+
     transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
     fragment.arguments = bundle
     transaction.commit()
