e0f2bcc209823cf6634fb98d13561dbe7d6af2db
atiQkhalid <39992488+atiQkhalid@users.noreply.github.com>
2021-11-14 00:44:34 +0500

    API call for Detailed page

--- app/src/main/java/com/example/sampleprojectforrecruitment/network/APIInterface.kt
+++ app/src/main/java/com/example/sampleprojectforrecruitment/network/APIInterface.kt
@@ -1,5 +1,6 @@
 package com.example.sampleprojectforrecruitment.network
 
+import com.example.sampleprojectforrecruitment.models.response.CountryDetailResponse
 import com.example.sampleprojectforrecruitment.models.response.CountryResponse
 import retrofit2.Call
 import retrofit2.http.GET
@@ -8,6 +9,10 @@ import retrofit2.http.GET
  * The APIInterface.kt
  */
 interface ApiInterface {
+
     @GET("countries")
     fun getAllCountries(): Call<CountryResponse>
+
+    @GET("statistics")
+    fun getCountryDetail(): Call<CountryDetailResponse>
 }
