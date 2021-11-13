d439cbc650d94a25185a2ecb9049135a16d88dfd
atiQkhalid <39992488+atiQkhalid@users.noreply.github.com>
2021-11-14 00:46:31 +0500

    constants added

--- app/src/main/java/com/example/sampleprojectforrecruitment/utils/Constants.kt
+++ app/src/main/java/com/example/sampleprojectforrecruitment/utils/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
     const val BASE_URL = "https://covid-193.p.rapidapi.com/"
     const val HOST_KEY = "covid-193.p.rapidapi.com"
     const val API_KEY = "49d30fbe46msh561521622491b98p1d11edjsn0b6e66609ba3"
+    const val COUNTRY_NAME = "country_name"
 }
